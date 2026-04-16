55a94671f9d88a61d8a5fa563f442ba12333a7aa
RocioMaSala <martinrocio16@gmail.com>
2026-04-16 17:37:27 +0200

    16.4

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -1,14 +1,47 @@
 import random
 
+achievements = [
+    'Crafting Genius', 'Strategist', 'World Savior',
+    'Speed Runner', 'Survivor',
+    'Master Explorer', 'Treasure Hunter', 'Unstoppable',
+    'First Steps', 'Collector Supreme',
+    'Untouchable', 'Sharp Mind', 'Boss Slayer'
+]
 
-def gen_player_achievements():
-    
-
-
-
-
+def gen_player_achievements() -> set[str]:
+    return set(random.sample(achievements, random.randint(1, len(achievements))))
 
 if __name__ == "__main__":
     print("=== Achievement Tracker System ===\n")
-    for player in [Alice, Bob, Charlie, Dylan]:
-        print(f"Player {player}: {gen_player_achievements}")
+    all_sets = {} #Creo un diccionario de sets, no una lista. 
+    for player in ['Alice', 'Bob', 'Charlie', 'Dylan']: 
+        player_achievements = gen_player_achievements()
+        all_sets[player] = player_achievements
+        print(f"Player {player}: {player_achievements}")
+    print()
+
+    distinct_achievements = set.union(*all_sets.values())
+    print(f"All distinct achievements: {distinct_achievements}\n")
+
+    common_achievements = set.intersection(*all_sets.values())
+    print(f"Common achievements: {common_achievements}\n")
+
+    unique_achievements = {}
+    for player in all_sets:
+            others = set().union(*(v for p, v in all_sets.items() if p != player))
+            unique_achievements[player] = all_sets[player].difference(others)
+            print(f"Only {player} has: {unique_achievements[player]}")
+
+    print()
+    missing_achievements = {}
+    for player in all_sets:
+            others = set().union(*(v for p, v in all_sets.items() if p != player))
+            missing_achievements[player] = others.difference(all_sets[player])
+            print(f"{player} is missing: {missing_achievements[player]}")
+
+
+
+
+
+
+
---
+++ ex4/ft_inventory_system.py
@@ -0,0 +1,46 @@
+import sys
+
+
+inventory: dict [str, int] = {}
+
+if __name__ == "__main__":
+    print("=== Inventory System Analysis ===")
+    for arg in sys.argv[1:]:
+        try:
+            item, quantity_str = arg.split(":")
+        except ValueError:
+            print(f"Error - invalid parameter {arg}")
+            continue
+        try:
+            quantity = int(quantity_str)
+        except ValueError as e:
+            print(f"Quantity error for '{item}': {e}")
+            continue
+        if item in inventory:
+            print(f"Redundant item {item} - discarding")
+            continue
+        inventory[item] = quantity
+    
+    print(f"Got inventory: {inventory}")
+    
+    print(f"Item list: {list(inventory.keys())}")
+    
+    total_quantity = sum(inventory.values())
+    item_number = len(inventory.keys())
+    print(f"Total quantity of the {item_number} items: {total_quantity}")
+
+    if item_number > 0:
+        for item, quantity in inventory.items():
+            representation = quantity / total_quantity * 100
+            print(f"Item {item} represents {representation:.2f}%")
+
+        most_abundant_item = max(inventory.items(), key=lambda x: x[1])
+        item, quantity = most_abundant_item #Hay que desempaquetar para definir item y q
+        print(f"Item most abundant: {item} with quantity {quantity}")
+
+        least_abundant_item = min(inventory.items(), key=lambda x: x[1])
+        item, quantity = least_abundant_item
+        print(f"Item least abundant: {item} with quantity {quantity}")
+
+    inventory['magic_item'] = 1
+    print(f"Updated inventory: {inventory}")
